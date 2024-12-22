267cb28cf8442074582337ea437359852987587f
ozkoka22 <ozkoka22@itu.edu.tr>
2024-12-23 01:27:31 +0300

    Added DatabaseHelper.cs waiting for sql connection

---
+++ RestaurantGo/DataAccess/DatabaseHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace RestaurantGo.DataAccess
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string connectionString = "Server=YOUR_SERVER;Database=RestaurantGo;Trusted_Connection=True;";
+
+        public static DataTable ExecuteQuery(string query, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                DataTable dataTable = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+
+                return dataTable;
+            }
+        }
+
+        public static void ExecuteNonQuery(string query, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
